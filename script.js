3a390972db43cfc6bf9651ace5fd7a4ae9c5590d
elyormuhandis <elyormuhandis@gmail.com>
2022-12-27 21:39:02 +0500

    finished

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 const modal = document.querySelector('.modal-window')
 const modalBtns = document.querySelectorAll('.show-modal-window')
 const overlay = document.querySelector('.overlay')
+const closeBtn = document.querySelector('.close-modal-window')
 const showModalHandler = () => {
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden')
     
 }
-
 const hideModalHandler = () => {
     modal.classList.add('hidden')
     overlay.classList.add('hidden')
@@ -19,3 +19,4 @@ for (const btn of modalBtns) {
 }
 
 overlay.addEventListener('click', hideModalHandler)
+closeBtn.addEventListener('click', hideModalHandler)
